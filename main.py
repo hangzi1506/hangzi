e6081a27cb5e9c3d5a3d3af7b21b48ba726f8ef0
hangzi <hangzi1506@126.com>
2019-08-15 21:31:15 +0800

    第五次提交:标题

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ import sys
 def start():
     pygame.init()
     window=pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-
+    pygame.display.set_caption("坦克大战")
+    #加载大本营
+    iconImage=pygame.image.load('./img/camp.gif')
+    pygame.display.set_icon(iconImage)
     while True:
+        #xianshi
+
         eventList=pygame.event.get()
         for eventEle in eventList:
             if eventEle.type==QUIT:
